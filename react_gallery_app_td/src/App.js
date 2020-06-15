71a634cf24ae6572ea79d5a65169a6a30c23584d
harshay <harshaykr@hotmail.com>
2020-06-15 12:27:56 +0100

    update
    update

---
+++ react_gallery_app_td/src/App.js
@@ -0,0 +1,23 @@
+/*********************************************************************************************************
+ * Techdegree 
+ * Project 7 - Gallery React App 
+ * Exceeds Expectation Grade
+ * Full Stack Javascript - Treehouse
+ * Component - Main app container
+ * Harshay 
+ *********************************************************************************************************/
+
+import React, { Component } from 'react';
+import searchform from './components/searchform';
+
+
+function App() {
+  return (
+    <div className="container">
+      <searchform /> 
+
+    </div>
+  );
+}
+ 
+export default App;
